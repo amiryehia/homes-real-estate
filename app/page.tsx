c0d3552c0400bf411baa415db68bd15b979fb7c9
Amir Ahmed <your-github-email@example.com>
2026-08-04 23:18:47 +0300

    Update website

--- app/page.tsx
+++ app/page.tsx
@@ -292,7 +292,7 @@ export default function Home() {
             <div className="office-image">
 
               <Image
-                src="/images/office-main2.jpg"
+                src="/images/office-main.jpg"
                 alt="Office Building"
                 width={650}
               
