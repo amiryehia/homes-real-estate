c7b53cdb38b05c3ff613f4100add2039f114fedd
Amir Ahmed <your-github-email@example.com>
2026-08-04 22:04:29 +0300

    Changed title to HOMES

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Prime Office",
+  title: "homes-real-estate",
   description: "Commercial Real Estate in Egypt",
 };
 
